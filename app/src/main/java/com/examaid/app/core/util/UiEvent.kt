058bbf544806a319faf791a9096c6ca5316418ff
burak kaytaz <burakkaytaz@gmail.com>
2025-11-16 16:23:46 +0300

    first commit

--- app/src/main/java/com/examaid/app/core/util/UiEvent.kt
+++
@@ -1,8 +0,0 @@
-package com.examaid.app.core.util
-
-sealed class UiEvent {
-    data class ShowSnackbar(val message: String) : UiEvent()
-    data class Navigate(val route: String) : UiEvent()
-    data object NavigateUp : UiEvent()
-}
-
--- app/src/main/java/com/examaid/app/domain/model/PhotoReason.kt
+++
@@ -1,8 +0,0 @@
-package com.examaid.app.domain.model
-
-enum class PhotoReason {
-    NEW_LEARNING,
-    GOOD_QUESTION,
-    COULD_NOT_SOLVE
-}
-
